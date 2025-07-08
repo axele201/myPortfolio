5db7fdd14ac7e33cccc969cc1e47ea8d671e9cd6
user <user@MacBook-Pro-user.local>
2025-07-08 18:06:30 +0700

    Updates

--- assets/index-C72MSdQm.js
+++ assets/index-C72MSdQm.js
@@ -3,8 +3,10 @@ import{initializeApp as j}from"https://www.gstatic.com/firebasejs/11.10.0/fireba
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16 items-center">
           <div class="flex items-center space-x-2">
-            <img src="/myPortfolio/DIA.png" alt="Logo Perusahaan" class="h-8 w-8">
-            <span class="text-xl font-semibold text-gray-900">Portfolio</span>
+            <a href="#/portfolio">
+              <img src="/myPortfolio/DIA.png" alt="Logo Perusahaan" class="h-8 w-8">
+              <span class="text-xl font-semibold text-gray-900">Portfolio</span>
+            </a>
           </div>
 
           <div class="hidden md:flex space-x-6">
